c0c2e517b3d81d324e449c6a06f9ccf881c94d19
huanghj <25813111@qq.com>
2022-06-09 09:40:18 +0800

    add html portlet

--- src/components/dashboard/board.vue
+++ src/components/dashboard/board.vue
@@ -1,46 +1,67 @@
-
 <template>
-    <q-page class="q-pa-sm">
-        <div class="row q-col-gutter-lg">
-            <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-                <widget-rows appid="users" title="女性员工" :params="{
-                    gender: ['1']
-                }">
-                </widget-rows>
-                
-                <widget-rows appid="users" title="黄姓员工" :params="{
-                    name: '黄'
-                }">
-                </widget-rows>           
-            </div>
-            <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-                <widget-rows appid="users" title="男性员工" :params="{
-                    gender: ['0']
-                }">
-                </widget-rows>                
-            </div>
-            <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-                <widget-rows appid="users" title="30-40岁员工" :params="{
-                    age: {
-                        min: 30,
-                        max: 40
-                    }
-                }">
-                </widget-rows>                
-            </div>                        
-        </div>
-    </q-page>
+  <q-page class="q-pa-sm">
+    <div class="row q-col-gutter-lg">
+      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+        <widget-html
+          title="简介"
+          html="员工总数6000，男性员工5000人，女性1000人。"
+        >
+        </widget-html>
+        <widget-rows
+          appid="users"
+          title="女性员工"
+          :params="{
+            gender: ['1'],
+          }"
+        >
+        </widget-rows>
+
+        <widget-rows
+          appid="users"
+          title="黄姓员工"
+          :params="{
+            name: '黄',
+          }"
+        >
+        </widget-rows>
+      </div>
+      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+        <widget-rows
+          appid="users"
+          title="男性员工"
+          :params="{
+            gender: ['0'],
+          }"
+        >
+        </widget-rows>
+      </div>
+      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+        <widget-rows
+          appid="users"
+          title="30-40岁员工"
+          :params="{
+            age: {
+              min: 30,
+              max: 40,
+            },
+          }"
+        >
+        </widget-rows>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 import WidgetRows from "./rows.vue";
-
+import WidgetHtml from "./html.vue";
 
 export default defineComponent({
-    name: 'DashBoard',
-    components: {
-        WidgetRows
-    }
+  name: "DashBoard",
+  components: {
+    WidgetRows,
+    WidgetHtml,
+  },
 });
 </script>
---
+++ src/components/dashboard/html.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card bordered class="q-mt-lg">
+    <q-card-section>
+      <div class="text-h6">{{ title }}</div>
+    </q-card-section>
+    <q-separator />
+    <q-card-section>
+      <div v-html="html"></div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WidgetHtml",
+  props: ["title", "html"],
+  data: function () {
+    return {};
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
