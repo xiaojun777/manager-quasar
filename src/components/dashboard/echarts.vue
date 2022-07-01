adf4706fa5ab407e1135d8b8b9b2bec93bee778b
Max <260861886@qq.com>
2022-07-01 09:28:31 +0800

    添加echarts表格

---
+++ src/components/dashboard/echarts.vue
@@ -0,0 +1,94 @@
+<template>
+  <board-widget
+    :title="innerPortlet.title"
+    :titleshow="innerPortlet.titleshow"
+    @edit="onEdit"
+    @delete="onDelete"
+    :editable="editable"
+  >
+    <div ref="echarts" id="echarts" style="height: 250px"></div>
+  </board-widget>
+  <portlet-editor
+    v-model:title="innerPortlet.title"
+    v-model:titleshow="innerPortlet.titleshow"
+    @save="onEditorSave"
+    @cancel="onEditorCancel"
+    v-model:show="editorShow"
+  >
+    <q-input
+      v-for="item in datas"
+      type="number"
+      :key="item.name"
+      v-model.number="item.value"
+      :label="item.name"
+    ></q-input>
+  </portlet-editor>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import BoardWidget from './widget.vue'
+import Ckeditor from '../base/ckeditor.vue'
+import portletbase from '../mixins/portletbase'
+import editorbase from '../mixins/editorbase'
+import PortletEditor from './portleteditor.vue'
+import * as echarts from 'echarts'
+
+export default defineComponent({
+  name: 'WidgetECharts',
+  props: [],
+  mixins: [portletbase, editorbase],
+  components: {
+    BoardWidget,
+    // Ckeditor,
+    PortletEditor
+  },
+  data: function () {
+    return {
+      model: false,
+      chart: null,
+      datas: null
+    }
+  },
+  emits: {
+    delete: null
+  },
+  watch: {
+    datas: {
+      handler(val) {
+        console.log('datas ', val)
+        this.init()
+
+        // this.init()
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    this.datas = this.innerPortlet.params.options.series[0].data
+    this.init()
+  },
+  methods: {
+    onDelete() {
+      this.$emit('delete', this.portlet.name)
+    },
+    init() {
+      let lineChart = this.$refs['echarts']
+      console.log('echarts:', lineChart)
+      if (lineChart !== null) {
+        // echarts.dispose(lineChart)
+        let theme = this.model ? 'dark' : 'light'
+        lineChart = echarts.init(lineChart, theme)
+        lineChart.setOption(this.innerPortlet.params.options)
+      }
+    },
+    onResize() {
+      if (this.line_chart) {
+        this.line_chart.resize()
+      }
+    }
+  }
+})
+</script>
+
+<style lang="sass" scoped></style>
--- src/mock/pages/users.js
+++ src/mock/pages/users.js
@@ -1,185 +1,218 @@
-import mockhandler from './base/mockhandler';
+import mockhandler from './base/mockhandler'
 
-let rows = [{
-  userid: "zhangsan",
-  name: "张三",
-  age: 45,
-  gender: "0",
-  hobby: "骑行 徒步",
-}, {
-  userid: "lisi",
-  name: "李四",
-  age: 44,
-  gender: "0",
-  hobby: "跑步 徒步",
-}, {
-  userid: "wangwu",
-  name: "王五",
-  age: 34,
-  gender: "0",
-  hobby: "跑步 徒步 楚留香",
-}, {
-  userid: "liuyi",
-  name: "刘一",
-  age: 46,
-  gender: "0",
-  hobby: "跑步 徒步 骑行",
-}, {
-  userid: "chener",
-  name: "chener",
-  age: 45,
-  gender: "0",
-  hobby: "跑步 徒步 骑行 打牌 高尔夫",
-}, {
-  userid: "zhaoliu",
-  name: "赵六",
-  age: 45,
-  gender: "0",
-  hobby: "跑步 徒步 骑行 打牌 足球",
-}, {
-  userid: "sunqi",
-  name: "孙七",
-  age: 30,
-  gender: "1",
-  hobby: "跑步 徒步 骑行 喝酒",
-}, {
-  userid: "zhouba",
-  name: "周八",
-  age: 27,
-  gender: "1",
-  hobby: "跑步 徒步 骑行 刺绣",
-}, {
-  userid: "wujiu",
-  name: "吴九",
-  age: 45,
-  gender: "0",
-  hobby: "徒步 骑行 电影",
-}, {
-  userid: "zhengshi",
-  name: "郑十",
-  age: 27,
-  gender: "0",
-  hobby: "徒步 骑行 电影 台球 机车",
-}, {
-  userid: "haoyu",
-  name: "浩宇",
-  age: 24,
-  gender: "0",
-  hobby: "徒步",
-}, {
-  userid: "yize",
-  name: "奕泽",
-  age: 27,
-  gender: "0",
-  hobby: "徒步",
-}];
+let rows = [
+  {
+    userid: 'zhangsan',
+    name: '张三',
+    age: 45,
+    gender: '0',
+    hobby: '骑行 徒步'
+  },
+  {
+    userid: 'lisi',
+    name: '李四',
+    age: 44,
+    gender: '0',
+    hobby: '跑步 徒步'
+  },
+  {
+    userid: 'wangwu',
+    name: '王五',
+    age: 34,
+    gender: '0',
+    hobby: '跑步 徒步 楚留香'
+  },
+  {
+    userid: 'liuyi',
+    name: '刘一',
+    age: 46,
+    gender: '0',
+    hobby: '跑步 徒步 骑行'
+  },
+  {
+    userid: 'chener',
+    name: 'chener',
+    age: 45,
+    gender: '0',
+    hobby: '跑步 徒步 骑行 打牌 高尔夫'
+  },
+  {
+    userid: 'zhaoliu',
+    name: '赵六',
+    age: 45,
+    gender: '0',
+    hobby: '跑步 徒步 骑行 打牌 足球'
+  },
+  {
+    userid: 'sunqi',
+    name: '孙七',
+    age: 30,
+    gender: '1',
+    hobby: '跑步 徒步 骑行 喝酒'
+  },
+  {
+    userid: 'zhouba',
+    name: '周八',
+    age: 27,
+    gender: '1',
+    hobby: '跑步 徒步 骑行 刺绣'
+  },
+  {
+    userid: 'wujiu',
+    name: '吴九',
+    age: 45,
+    gender: '0',
+    hobby: '徒步 骑行 电影'
+  },
+  {
+    userid: 'zhengshi',
+    name: '郑十',
+    age: 27,
+    gender: '0',
+    hobby: '徒步 骑行 电影 台球 机车'
+  },
+  {
+    userid: 'haoyu',
+    name: '浩宇',
+    age: 24,
+    gender: '0',
+    hobby: '徒步'
+  },
+  {
+    userid: 'yize',
+    name: '奕泽',
+    age: 27,
+    gender: '0',
+    hobby: '徒步'
+  }
+]
 
 let app = {
-  id: "users",
-  label: "用户管理",
+  id: 'users',
+  label: '用户管理',
   schema: {
-    key: "userid",
-    apps: [{
-      label: '教育经历',
-      id: 'educations',
-      foreigns: ['userid']
-    }],
-    items: [{
-      id: "userid",
-      label: "用户ID",
-      type: "string", //string, option, number, date, time, month
-      default: "",
-      searchable: false,
-      sortable: true,
-      rules: [{
-        type: "required",
-      },{
-        type: "alpha",
-      },{
-        type: "minlength",
-        params: [6],
-      },{
-        type: "maxlength",
-        params: [12],
-      }],
-    },{
-      id: "name",
-      label: "姓名",
-      type: "string",
-      default: "",
-      searchable: true,
-      sortable: true,
-      rules: [{
-        type: "fullcode",
-      }],
-    },{
-      id: "age",
-      label: "年龄",
-      type: "number",
-      default: 1,
-      searchable: true,
-      sortable: true,
-      rules: [{
-        type: "min",
-        params: [0],
+    key: 'userid',
+    apps: [
+      {
+        label: '教育经历',
+        id: 'educations',
+        foreigns: ['userid']
+      }
+    ],
+    items: [
+      {
+        id: 'userid',
+        label: '用户ID',
+        type: 'string', //string, option, number, date, time, month
+        default: '',
+        searchable: false,
+        sortable: true,
+        rules: [
+          {
+            type: 'required'
+          },
+          {
+            type: 'alpha'
+          },
+          {
+            type: 'minlength',
+            params: [6]
+          },
+          {
+            type: 'maxlength',
+            params: [12]
+          }
+        ]
       },
       {
-        type: "max",
-        params: [150],
-      }],
-    },{
-      id: "minzu",
-      label: "民族",
-      type: "string",
-      default: "汉",
-      searchable: false,
-      sortable: true,
-      rules: [{
-        type: "fullcode",
-      }],
-    },{
-      id: "gender",
-      label: "性别",
-      type: "option",
-      default: 0,
-      searchable: true,
-      sortable: true,
-      options: {
-        0: "男",
-        1: "女",
+        id: 'name',
+        label: '姓名',
+        type: 'string',
+        default: '',
+        searchable: true,
+        sortable: true,
+        rules: [
+          {
+            type: 'fullcode'
+          }
+        ]
       },
-      rules: [],
-    },{
-      id: "hobby",
-      label: "爱好",
-      type: "string",
-      searchable: false,
-      sortable: true,
-      rules: [],
-    }],
+      {
+        id: 'age',
+        label: '年龄',
+        type: 'number',
+        default: 1,
+        searchable: true,
+        sortable: true,
+        rules: [
+          {
+            type: 'min',
+            params: [0]
+          },
+          {
+            type: 'max',
+            params: [150]
+          }
+        ]
+      },
+      {
+        id: 'minzu',
+        label: '民族',
+        type: 'string',
+        default: '汉',
+        searchable: false,
+        sortable: true,
+        rules: [
+          {
+            type: 'fullcode'
+          }
+        ]
+      },
+      {
+        id: 'gender',
+        label: '性别',
+        type: 'option',
+        default: 0,
+        searchable: true,
+        sortable: true,
+        options: {
+          0: '男',
+          1: '女'
+        },
+        rules: []
+      },
+      {
+        id: 'hobby',
+        label: '爱好',
+        type: 'string',
+        searchable: false,
+        sortable: true,
+        rules: []
+      }
+    ]
   }
-};
+}
 
-let searching = {};
-let retRows = [];
+let searching = {}
+let retRows = []
 
 export default {
   app: app,
   installData: function (mock) {
-    mock.onGet("/app/users/items").reply(function (config) {
-      return mockhandler.items(app, rows, retRows, searching, config);
-    });
+    mock.onGet('/app/users/items').reply(function (config) {
+      return mockhandler.items(app, rows, retRows, searching, config)
+    })
 
-    mock.onAny("/app/users/add").reply(function (config) {
-      return mockhandler.add(app, rows, config);
-    });
+    mock.onAny('/app/users/add').reply(function (config) {
+      return mockhandler.add(app, rows, config)
+    })
 
-    mock.onAny("/app/users/update").reply(function (config) {
-      return mockhandler.update(app, rows, config);
-    });
+    mock.onAny('/app/users/update').reply(function (config) {
+      return mockhandler.update(app, rows, config)
+    })
 
-    mock.onAny("/app/users/delete").reply(function (config) {
-      return mockhandler.delete(app, rows, config);
-    });
-  },
-};
+    mock.onAny('/app/users/delete').reply(function (config) {
+      return mockhandler.delete(app, rows, config)
+    })
+  }
+}
