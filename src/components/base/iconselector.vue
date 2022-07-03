f283f21bac92a197d45688e04707684b7cfae39c
xiaojun777 <25813111@qq.com>
2022-07-03 18:22:31 +0800

    add icons selector

---
+++ src/components/base/iconselector.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-table> </q-table>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { icons } from "./icons";
+export default defineComponent({
+  name: "IconSelector",
+  props: {},
+  components: {},
+  emits: {
+    "update:selected": null,
+  },
+  data: function () {
+    return {
+      icons: icons,
+    };
+  },
+  watch: {
+    editorData(val) {
+      this.$emit("update:selected", val);
+    },
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style></style>
