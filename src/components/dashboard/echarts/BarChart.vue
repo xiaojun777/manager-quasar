78dc5cc6b0f4049b56e1c0465af207ea02b30782
Max <260861886@qq.com>
2022-07-07 16:19:10 +0800

    添加主题适配

--- src/components/dashboard/echarts/BarChart.vue
+++ src/components/dashboard/echarts/BarChart.vue
@@ -1,29 +1,5 @@
 <template>
-  <q-btn @click="addData">增加</q-btn>
-  <div v-if="innerOptions">
-    <div
-      class="row"
-      v-for="(item, index) in innerOptions.series[0].data"
-      :key="index"
-    >
-      <q-input
-        class="col-3 q-mr-sm"
-        v-model="innerOptions.xAxis.data[index]"
-        label="名称"
-      ></q-input>
-      <q-input class="col-3 q-mr-sm" v-model="item.value" label="值"></q-input>
-      <q-input class="col-2 q-mr-sm" filled v-model="item.itemStyle.color">
-        <template v-slot:append>
-          <q-icon name="colorize" class="cursor-pointer">
-            <q-popup-proxy transition-show="scale" transition-hide="scale">
-              <q-color v-model="item.itemStyle.color" />
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-btn @click="deleteRow(index)">删除</q-btn>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -33,7 +9,8 @@ import axios from 'axios'
 export default defineComponent({
   name: 'BarChart',
   props: {
-    options: Object
+    options: Object,
+    type: String
   },
   watch: {
     innerOptions: {
@@ -54,33 +31,17 @@ export default defineComponent({
   computed: {},
 
   mounted: async function () {
-    if (this.options == null) {
+    if (this.options === null || this.options.series[0].type !== this.type) {
       let response = await this.getBarOptions()
-      let arr = []
-      response.series[0].data.forEach((item) => {
-        arr.push({ value: item, itemStyle: { color: null } })
-      })
-      response.series[0].data = arr
       this.innerOptions = response
     }
   },
   methods: {
     async getBarOptions() {
       let response = await axios.get('echarts', {
-        params: { board: 'bar' }
+        params: { board: this.type }
       })
       return response.data
-    },
-    deleteRow(index) {
-      this.innerOptions.series[0].data.splice(index, 1),
-        this.innerOptions.xAxis.data.splice(index, 1)
-    },
-    addData() {
-      this.innerOptions.xAxis.data.push('')
-      this.innerOptions.series[0].data.push({
-        value: '',
-        itemStyle: { color: null }
-      })
     }
   }
 })
--- src/components/dashboard/echarts/PieChart.vue
+++ src/components/dashboard/echarts/PieChart.vue
@@ -1,30 +1,5 @@
 <template>
-  <q-btn @click="addData">增加</q-btn>
-  <div v-if="innerOptions">
-    <div
-      class="row"
-      v-for="(item, index) in innerOptions.series[0].data"
-      :key="index"
-    >
-      <q-input class="col-3 q-mr-sm" label="名称" v-model="item.name"></q-input>
-      <q-input
-        class="col-3 q-mr-sm"
-        label="值"
-        type="number"
-        v-model.number="item.value"
-      ></q-input>
-      <q-input class="col-2 q-mr-sm" filled v-model="item.itemStyle.color">
-        <template v-slot:append>
-          <q-icon name="colorize" class="cursor-pointer">
-            <q-popup-proxy transition-show="scale" transition-hide="scale">
-              <q-color v-model="item.itemStyle.color" />
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-btn @click="deleteRow(index)">删除</q-btn>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -34,7 +9,8 @@ import axios from 'axios'
 export default defineComponent({
   name: 'PieChart',
   props: {
-    options: Object
+    options: Object,
+    type: String
   },
   watch: {
     innerOptions: {
@@ -49,36 +25,22 @@ export default defineComponent({
   },
   data() {
     return {
-      innerOptions: this.options !== void 0 ? this.options : null,
-      color: ''
+      innerOptions: this.options !== void 0 ? this.options : null
     }
   },
   computed: {},
 
   mounted: async function () {
-    if (this.options == null) {
+    if (this.options == null || this.options.series[0].type !== this.type) {
       this.innerOptions = await this.getPieOptions()
-      if (this.innerOptions.series[0].data) {
-        this.innerOptions.series[0].data.forEach((item) => {
-          item.itemStyle = { color: undefined }
-          // if(item.itemStyle && !item.it)
-        })
-      }
     }
   },
   methods: {
     async getPieOptions() {
       let response = await axios.get('echarts', {
-        params: { board: 'pie' }
+        params: { board: this.type }
       })
       return response.data
-    },
-    addData() {
-      let element = { name: '', value: 0, itemStyle: { color: '' } }
-      this.innerOptions.series[0].data.push(element)
-    },
-    deleteRow(index) {
-      this.innerOptions.series[0].data.splice(index, 1)
     }
   }
 })
